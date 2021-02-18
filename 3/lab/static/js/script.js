1d00bec05747267fb89b4b909c575cfcd8ca344d
ernurator <isenbaev121102ernur@gmail.com>
2021-02-18 13:11:17 +0600

    added saving data to local storage

--- 3/lab/static/js/script.js
+++ 3/lab/static/js/script.js
@@ -2,15 +2,28 @@ const submitBtn = document.querySelector('.add-btn');
 const itemsList = document.querySelector('.items');
 const input = document.querySelector('.new-task');
 
+window.onload = function() {
+    let items = JSON.parse(localStorage.getItem('data-list'));
+    if(items) {
+        items.forEach(item => addElement(item.text, item.checked));
+    }
+};
+
 submitBtn.addEventListener('click', function(){
     const txt = input.value.trim();
     input.value = "";
     if (txt == "") return;
+    addElement(txt);
+    saveChanges();
+});
+
+function addElement(txt, checked=false) {
     const taskTxt = document.createElement('span');
     taskTxt.innerHTML = txt;
 
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
+    checkbox.checked = checked;
     checkbox.addEventListener('click', changeDecoration);
 
     const binImg = document.createElement('img')
@@ -19,16 +32,17 @@ submitBtn.addEventListener('click', function(){
     binImg.addEventListener('click', deleteItem);
 
     const task = document.createElement('div');
-    task.className = 'item';
+    task.className = 'item ' + (checked ? 'checked' : 'unchecked');
     task.appendChild(checkbox);
     task.appendChild(taskTxt);
     task.appendChild(binImg);
 
     itemsList.appendChild(task);
-});
+}
 
 function deleteItem() {
     this.parentNode.remove();
+    saveChanges();
 }
 
 function changeDecoration() {
@@ -36,4 +50,20 @@ function changeDecoration() {
         this.parentNode.className = 'item checked';
     else
         this.parentNode.className = 'item unchecked';
+    saveChanges();
+}
+
+function saveChanges() {
+    let dataList = []
+    itemsList.childNodes.forEach(node => {
+        let checked = (node.querySelector('input') || {}).checked;
+        let text = (node.querySelector('span') || {}).innerHTML;
+        if(checked === undefined || !text) return;
+        dataList.push({
+            checked,
+            text
+        });
+        console.log(node);
+    });
+    localStorage.setItem('data-list', JSON.stringify(dataList));
 }
