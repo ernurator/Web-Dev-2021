fb1176d5a5f7e7af9a82354f7a66baedefdb2c77
ernurator <isenbaev121102ernur@gmail.com>
2021-03-06 16:12:28 +0600

    deleted comment

--- 4/lab/my-store/src/app/app.component.ts
+++ 4/lab/my-store/src/app/app.component.ts
@@ -2,30 +2,6 @@ import { Component } from '@angular/core';
 import categories from './categories.json'
 import { Category } from './category'
 
-// export class Item {
-//   name: string;
-//   link: string;
-//   imageLink: string;
-//   description: string;
-//   rating: number;
-//   categoryId: number;
-//   likes: number;
-//   constructor(data: any) {
-//     this.name = data?.name || '';
-//     this.link = data?.link || '';
-//     this.imageLink = data?.imageLink || '';
-//     this.description = data?.description || '';
-//     this.rating = data?.rating || 0;
-//     this.categoryId = data?.categoryId || 0;
-//     this.likes = data?.likes || 0;
-//   }
-// }
-
-// export interface Category {
-//   id: number,
-//   name: string
-// }
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
